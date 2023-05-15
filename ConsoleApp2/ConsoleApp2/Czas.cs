072921e6a08cda399c251b4d43a40487918de6c7
xkowalczyk <mkowalczykprogram@gmail.com>
2023-05-15 19:21:21 +0200

    init, konstruktor zerujący

---
+++ ConsoleApp2/ConsoleApp2/Czas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Czas
+    {
+        public int s = 0;
+        public int m = 0;
+        public int h = 0;
+
+        Czas()
+        {
+            s = 0;
+            m = 0;
+            h = 0;
+        }
+    }
+}
